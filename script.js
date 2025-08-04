be922dcb75572b030f5f568754f0711da4693f5b
Dede Tugi Waluyo <153931479+absolutelyrichd@users.noreply.github.com>
2025-08-04 14:43:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<!-- Firebase SDK & App Logic -->
     <script type="module">
         import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
         import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
