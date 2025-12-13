ec2633ac3d8f5c6c8bcc189d129781a715556485
Dede Tugi Waluyo <dtugi46@gmail.com>
2025-12-13 09:41:08 +0700

    intergrasi pwa lg

--- script.js
+++ script.js
@@ -955,3 +955,12 @@ if (navbar) {
         }
     });
 }
+
+// Cek apakah browser mendukung Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('Service Worker Registered!', reg))
+      .catch(err => console.log('Service Worker Registration Failed:', err));
+  });
+}
